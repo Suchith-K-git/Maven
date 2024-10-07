0b605b9d956f9da3518b43c536216f232e39e0cf
Suchith-K-git <suchithk30@gmail.com>
2024-10-07 09:56:22 +0530

    Update App.java

--- src/main/java/suchith/App.java
+++ src/main/java/suchith/App.java
@@ -6,8 +6,6 @@ package suchith;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-         System.out.println("Hello World!");
-        System.out.println("Hello World!");
 
     }
 }
